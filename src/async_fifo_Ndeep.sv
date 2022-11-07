f03471dd62ed3c8b9eea528398bb5a17ea9644f5
AdDraw <drawc.adam.priv@gmail.com>
2022-11-07 19:41:48 +0100

    small commit

--- src/async_fifo_Ndeep.sv
+++ src/async_fifo_Ndeep.sv
@@ -6,7 +6,7 @@
   GRAY encoded ptr -> not needed if you have a 2 deep fifo (not enough addresses)
   # write the data into the register if this register is not full
   # read from the register using clkB if register is not empty
-  
+
   Asynchronous Circular buffer N words deep:
   - wr_ptr & clkb_rd_ptr in binary
   - converted to GRAY and send with the use of 2FF sync over to the other clock domain
@@ -19,8 +19,8 @@
 `timescale 1ns/1ps
 
 module async_fifo_Ndeep #(
-    parameter DATA_WIDTH         = 8,
-    parameter BUFFER_DEPTH_POWER = 2
+    parameter int DATA_WIDTH         = 8,
+    parameter int BUFFER_DEPTH_POWER = 2
   )(
     // WRITE PORT CLKA
     input                     clkA_i,
@@ -35,19 +35,19 @@ module async_fifo_Ndeep #(
     output [DATA_WIDTH-1 : 0] cB_dout_o,
     output                    cB_rrdy_o
   );
-  localparam BUFFER_DEPTH = 2**BUFFER_DEPTH_POWER;
+  localparam int BufferDepth = 2**BUFFER_DEPTH_POWER;
 
   // INTER DOMAIN FIFO
-  logic [DATA_WIDTH-1 : 0 ] fifo [BUFFER_DEPTH];
+  logic [DATA_WIDTH-1 : 0 ] fifo [BufferDepth];
 
   // CLK A DOMAIN
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cA_wr_ptr;
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cA_rd_ptr_gray_sync;
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cA_rd_ptr_bin;
+  logic [$clog2(BufferDepth)-1 : 0] cA_wr_ptr;
+  logic [$clog2(BufferDepth)-1 : 0] cA_rd_ptr_gray_sync;
+  logic [$clog2(BufferDepth)-1 : 0] cA_rd_ptr_bin;
 
   logic cA_full  = (cA_wr_ptr + 1'b1 == cA_rd_ptr_bin) ? 1'b1 : 1'b0;
 
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cA_wr_ptr_gray = cA_wr_ptr ^ (cA_wr_ptr >> 1);
+  logic [$clog2(BufferDepth)-1 : 0] cA_wr_ptr_gray = cA_wr_ptr ^ (cA_wr_ptr >> 1);
 
   synchronizer_2ff #(
     .DATA_WIDTH(DATA_WIDTH)
@@ -59,7 +59,7 @@ module async_fifo_Ndeep #(
   );
 
   gray2bin #(
-    .DATA_WIDTH($clog2(BUFFER_DEPTH))
+    .DATA_WIDTH($clog2(BufferDepth))
   ) rd_ptr_gray2bin (
     .gray_i(cA_rd_ptr_gray_sync),
     .bin_o(cA_rd_ptr_bin)
@@ -82,14 +82,14 @@ module async_fifo_Ndeep #(
   // CLKB DOMAIN
 
   logic [DATA_WIDTH-1:0]             cB_dout;
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cB_rd_ptr;
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cB_wr_ptr_gray_sync; // driven by CLKA
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cB_wr_ptr_bin;
+  logic [$clog2(BufferDepth)-1 : 0] cB_rd_ptr;
+  logic [$clog2(BufferDepth)-1 : 0] cB_wr_ptr_gray_sync; // driven by CLKA
+  logic [$clog2(BufferDepth)-1 : 0] cB_wr_ptr_bin;
 
   logic cB_empty = (cB_wr_ptr_bin == cB_rd_ptr) ? 1'b1: 1'b0;
 
   // SEND RD PTR as GRAY
-  logic [$clog2(BUFFER_DEPTH)-1 : 0] cB_rd_ptr_gray = cB_rd_ptr ^ (cB_rd_ptr >> 1);
+  logic [$clog2(BufferDepth)-1 : 0] cB_rd_ptr_gray = cB_rd_ptr ^ (cB_rd_ptr >> 1);
 
   // READ WHEN NOT EMPTY
   always_ff @( posedge clkB_i or negedge cB_rst_ni ) begin
@@ -115,7 +115,7 @@ module async_fifo_Ndeep #(
     .data_sync_o(cB_wr_ptr_gray_sync)
   );
   gray2bin #(
-    .DATA_WIDTH($clog2(BUFFER_DEPTH))
+    .DATA_WIDTH($clog2(BufferDepth))
   ) wr_ptr_gray2bin (
     .gray_i(cB_wr_ptr_gray_sync),
     .bin_o(cB_wr_ptr_bin)
